e5deab09f5e2cbe04591c2e9eb170a2f6db93832
AvinashRay1702 <avinashkumar.ray@pennywisesolutions.com>
2025-01-24 13:40:55 +0530

    removed duplicate keys

--- scripts/mock-data.js
+++ scripts/mock-data.js
@@ -53,41 +53,6 @@ export const MOCK_CONTENT = {
 </div>
  
 </div></div>`,
-  'about-us':`<div class="about-us-wrapper"><div data-aue-resource="urn:aemconnection:/content/genting-singapore/index/TestPage1/jcr:content/root/section_2/block_1717109353" data-aue-type="container" data-aue-behavior="component" data-aue-model="aboutus" data-aue-label="About Us" data-aue-filter="aboutus" class="about-us block" style="" data-block-name="about-us" data-block-status="loaded">
-<div>
-<div><p data-aue-prop="title" data-aue-label="Title" data-aue-type="text">About Us</p></div>
-</div>
-<div>
-<div><p data-aue-prop="heading" data-aue-label="Heading" data-aue-type="text">An Industry Pioneer</p></div>
-</div>
-<div>
-<div><div data-aue-prop="sub-heading" data-aue-label="Sub Heading" data-aue-filter="text" data-aue-type="richtext"><p>We are uniquely positioned to enable long-term sustainable growth,both locally and globally, through integrated resort developments.</p></div></div>
-</div>
-<div data-aue-resource="urn:aemconnection:/content/genting-singapore/index/TestPage1/jcr:content/root/section_2/block_1717109353/item_1092409410" data-aue-type="component" data-aue-model="linkfield" data-aue-label="Link Field">
-<div><p class="button-container"><a href="/content/genting-singapore/index/about-us.html" title="About Us Page" data-aue-prop="linkText" data-aue-label="Text" data-aue-type="text" class="button">/content/genting-singapore/index/about-us</a></p></div>
-<div><p data-aue-prop="linkTarget" data-aue-label="Link Target" data-aue-type="text">_blank</p></div>
-</div>
-<div data-aue-resource="urn:aemconnection:/content/genting-singapore/index/TestPage1/jcr:content/root/section_2/block_1717109353/item" data-aue-type="component" data-aue-model="aboutusdescription" data-aue-label="About Us Description">
-<div></div>
-<div></div>
-<div><div data-aue-prop="feature-title" data-aue-label="Feature Title" data-aue-filter="text" data-aue-type="richtext"><p>S$10B</p></div></div>
-<div><p data-aue-prop="feature-heading" data-aue-label="Feature Heading" data-aue-type="text">In market cap</p></div>
-</div>
-<div data-aue-resource="urn:aemconnection:/content/genting-singapore/index/TestPage1/jcr:content/root/section_2/block_1717109353/item_351800267" data-aue-type="component" data-aue-model="aboutusdescription" data-aue-label="About Us Description">
-<div></div>
-<div></div>
-<div><div data-aue-prop="feature-title" data-aue-label="Feature Title" data-aue-filter="text" data-aue-type="richtext"><p>1st</p></div></div>
-<div><p data-aue-prop="feature-heading" data-aue-label="Feature Heading" data-aue-type="text">of the only 2 integrated resorts in Singapore</p></div>
-</div>
-<div data-aue-resource="urn:aemconnection:/content/genting-singapore/index/TestPage1/jcr:content/root/section_2/block_1717109353/item_1811588751" data-aue-type="component" data-aue-model="aboutusdescription" data-aue-label="About Us Description">
-<div></div>
-<div></div>
-<div><div data-aue-prop="feature-title" data-aue-label="Feature Title" data-aue-filter="text" data-aue-type="richtext"><p>10</p><p>INDEXES</p></div>
-</div>
-<div><p data-aue-prop="feature-heading" data-aue-label="Feature Heading" data-aue-type="text">We are a constituent of multiple indexes within the Singapore and global markets</p></div>
-</div>
- 
-</div></div>`
   // Add other block mock content
 };
 
