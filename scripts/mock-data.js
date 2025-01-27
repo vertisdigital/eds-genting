27c6fb67edd844b7c252ba45cc05e9dd6c1a8a85
Brajesh Kumar Sharma <brajeshkumar.sharma@verticurl.com>
2025-01-27 20:31:26 +0530

    review comments fixed

--- scripts/mock-data.js
+++ scripts/mock-data.js
@@ -1,4 +1,41 @@
 export const MOCK_CONTENT = {
+  'about-us': `<div class="about-us-wrapper"><div data-aue-resource="urn:aemconnection:/content/genting-singapore/index/TestPage1/jcr:content/root/section_2/block_1717109353" data-aue-type="container" data-aue-behavior="component" data-aue-model="aboutus" data-aue-label="About Us" data-aue-filter="aboutus" class="about-us block" style="" data-block-name="about-us" data-block-status="loaded">
+        <div>
+        <div><p data-aue-prop="title" data-aue-label="Title" data-aue-type="text">About Us</p></div>
+        </div>
+        <div>
+        <div><p data-aue-prop="heading" data-aue-label="Heading" data-aue-type="text">An Industry Pioneer</p></div>
+        </div>
+        <div>
+        <div><div data-aue-prop="sub-heading" data-aue-label="Sub Heading" data-aue-filter="text" data-aue-type="richtext"><p>We are uniquely positioned to enable long-term sustainable growth,both locally and globally, through integrated resort developments.</p></div></div>
+        </div>
+        <div data-aue-resource="urn:aemconnection:/content/genting-singapore/index/TestPage1/jcr:content/root/section_2/block_1717109353/item_1092409410" data-aue-type="component" data-aue-model="linkfield" data-aue-label="Link Field">
+        <div><p class="button-container"><a href="/content/genting-singapore/index/about-us.html" title="About Us Page" data-aue-prop="linkText" data-aue-label="Text" data-aue-type="text" class="button">/content/genting-singapore/index/about-us</a></p></div>
+        <div><p data-aue-prop="linkTarget" data-aue-label="Link Target" data-aue-type="text">_blank</p></div>
+        </div>
+        <div data-aue-resource="urn:aemconnection:/content/genting-singapore/index/TestPage1/jcr:content/root/section_2/block_1717109353/item" data-aue-type="component" data-aue-model="aboutusdescription" data-aue-label="About Us Description">
+        <div></div>
+        <div></div>
+        <div><div data-aue-prop="feature-title" data-aue-label="Feature Title" data-aue-filter="text" data-aue-type="richtext"><p>S$10B</p></div></div>
+        <div><p data-aue-prop="feature-heading" data-aue-label="Feature Heading" data-aue-type="text">In market cap</p></div>
+        </div>
+        <div data-aue-resource="urn:aemconnection:/content/genting-singapore/index/TestPage1/jcr:content/root/section_2/block_1717109353/item_351800267" data-aue-type="component" data-aue-model="aboutusdescription" data-aue-label="About Us Description">
+        <div></div>
+        <div></div>
+        <div><div data-aue-prop="feature-title" data-aue-label="Feature Title" data-aue-filter="text" data-aue-type="richtext"><p>1st</p></div></div>
+        <div><p data-aue-prop="feature-heading" data-aue-label="Feature Heading" data-aue-type="text">of the only 2 integrated resorts in Singapore</p></div>
+        </div>
+        <div data-aue-resource="urn:aemconnection:/content/genting-singapore/index/TestPage1/jcr:content/root/section_2/block_1717109353/item_1811588751" data-aue-type="component" data-aue-model="aboutusdescription" data-aue-label="About Us Description">
+        <div></div>
+        <div></div>
+        <div><div data-aue-prop="feature-title" data-aue-label="Feature Title" data-aue-filter="text" data-aue-type="richtext"><p>10</p><p>INDEXES</p></div>
+        </div>
+        <div><p data-aue-prop="feature-heading" data-aue-label="Feature Heading" data-aue-type="text">We are a constituent of multiple indexes within the Singapore and global markets</p></div>
+        </div>
+         
+        </div></div>`,
+    // Add other block mock content
+
   'hero-banner': `
   <div data-aue-resource="urn:aemconnection:/content/genting-singapore/index/herobannercarouseltest/jcr:content/root/section/block" data-aue-type="container" data-aue-behavior="component" data-aue-model="hero-banner" data-aue-label="hero-banner" data-aue-filter="hero-banner" class="hero-banner block" data-block-name="hero-banner" data-block-status="loaded">
     <div>
@@ -28,12 +65,12 @@ export const MOCK_CONTENT = {
     </div>
 <div>
         
-            <div><p><a href="https://author-p144202-e1488374.adobeaemcloud.com/content/dam/genting-singapore/herobanner/material-symbols_chevron-left%20(1).svg" title="https://author-p144202-e1488374.adobeaemcloud.com/content/dam/genting-singapore/herobanner/material-symbols_chevron-left%20(1).svg">https://author-p144202-e1488374.adobeaemcloud.com/content/dam/genting-singapore/herobanner/material-symbols_chevron-left%20(1).svg</a></p></div>
+            <div><p><a href="https://cdn.builder.io/api/v1/image/assets/TEMP/0effc117ceecce518dda1445ef1b1a8acb62bcff46f4e4bf7487f07e8adb7d38?placeholderIfAbsent=true&apiKey=16b1633103d8450ead7bc93647340540" title="https://cdn.builder.io/api/v1/image/assets/TEMP/0effc117ceecce518dda1445ef1b1a8acb62bcff46f4e4bf7487f07e8adb7d38?placeholderIfAbsent=true&apiKey=16b1633103d8450ead7bc93647340540">https://cdn.builder.io/api/v1/image/assets/TEMP/0effc117ceecce518dda1445ef1b1a8acb62bcff46f4e4bf7487f07e8adb7d38?placeholderIfAbsent=true&apiKey=16b1633103d8450ead7bc93647340540</a></p></div>
         
     </div>
 <div>
         
-            <div><p><a href="https://author-p144202-e1488374.adobeaemcloud.com/content/dam/genting-singapore/herobanner/material-symbols_chevron-left.svg" title="https://author-p144202-e1488374.adobeaemcloud.com/content/dam/genting-singapore/herobanner/material-symbols_chevron-left.svg">https://author-p144202-e1488374.adobeaemcloud.com/content/dam/genting-singapore/herobanner/material-symbols_chevron-left.svg</a></p></div>
+            <div><p><a href="https://cdn.builder.io/api/v1/image/assets/TEMP/46e7f6e7630ff9bebc63f97e43e2584d054bda17c8e90aabbeba89e1d721648b?placeholderIfAbsent=true&apiKey=16b1633103d8450ead7bc93647340540" title="https://cdn.builder.io/api/v1/image/assets/TEMP/46e7f6e7630ff9bebc63f97e43e2584d054bda17c8e90aabbeba89e1d721648b?placeholderIfAbsent=true&apiKey=16b1633103d8450ead7bc93647340540">https://cdn.builder.io/api/v1/image/assets/TEMP/46e7f6e7630ff9bebc63f97e43e2584d054bda17c8e90aabbeba89e1d721648b?placeholderIfAbsent=true&apiKey=16b1633103d8450ead7bc93647340540</a></p></div>
         
     </div>
 <div data-aue-resource="urn:aemconnection:/content/genting-singapore/index/herobannercarouseltest/jcr:content/root/section/block/item" data-aue-type="component" data-aue-model="bannercarousel" data-aue-label="Banner Carousel">
