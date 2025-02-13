b553a1ebc2d1ec2edf683d0e67f60bf0baf4795c
sudhir.madaan <sudhir.madaan@ogilvy.com>
2025-02-13 17:26:58 +0530

    Re-enable moveInstrumentation for tabs processing

--- scripts/autoblocks.js
+++ scripts/autoblocks.js
@@ -38,6 +38,8 @@ export default function processTabs(main, moveInstrumentation) {
  
   const topContainer = document.createElement('div');
   topContainer.classList = 'container-xl container-lg container-md container-sm';
+  moveInstrumentation(section, topContainer);
+
  
   const tabsWrapper = document.createElement('div');
   tabsWrapper.classList.add('tabs-container', 'block');
