7979710f0d4e9651a1ef13137521cd99d531c141
josephogilvy <joseph.ambrose@ogilvy.com>
2025-03-01 16:02:53 +0530

    milestone

--- blocks/historymilestones/historymilestones.js
+++ blocks/historymilestones/historymilestones.js
@@ -1,6 +1,17 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default function decorate(block) {
+  // Remove data-gen attributes
+  const elementsWithDataGen = block.querySelectorAll('[data-gen-prop], [data-gen-type], [data-gen-model], [data-gen-label]');
+  elementsWithDataGen.forEach(element => {
+    const attrs = element.attributes;
+    for(let i = attrs.length - 1; i >= 0; i--) {
+      if(attrs[i].name.startsWith('data-gen')) {
+        element.removeAttribute(attrs[i].name);
+      }
+    }
+  });
+
   // Process images
   const imageLinks = block.querySelectorAll('a[href*="delivery-"], a[href*="/adobe/assets/"]');
   imageLinks.forEach(link => {
