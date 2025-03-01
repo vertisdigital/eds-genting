12f63f01e2dae908f1c531d57b63c53920a56b39
sudhir.madaan <sudhir.madaan@ogilvy.com>
2025-03-01 18:31:45 +0530

    Update hero banner arrow icon link rendering

--- blocks/herobanner/herobanner.js
+++ blocks/herobanner/herobanner.js
@@ -108,7 +108,7 @@ export default function decorate(block) {
       color: '#B29152',
     });
     const parsedHtml = stringToHTML(arrowIconHtml);
-    // const anchorWrapper = document.createElement('a');
+    arrowIconLink.querySelector('a').textContent = '';
     arrowIconLink.querySelector('a')?.append(parsedHtml);
     heroContent.appendChild(arrowIconLink);
   }
