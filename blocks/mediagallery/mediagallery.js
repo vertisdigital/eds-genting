1703bf7ba2fe217940d017b08c93419e159580f4
manishverticurl <manish.thakur@verticurl.com>
2025-04-22 18:28:03 +0530

    modal implementation

--- blocks/mediagallery/mediagallery.js
+++ blocks/mediagallery/mediagallery.js
@@ -1,3 +1,6 @@
+import SvgIcon from '../../shared-components/SvgIcon.js';
+import stringToHTML from '../../shared-components/Utility.js';
+
 const imagesHeight={
     lg:{
         desktop: { height: "451px", width:"100%"},
@@ -56,17 +59,20 @@ export default function decorate(block) {
         img.alt = link.title || 'Gallery Image';
         img.style.width = imagesHeight.desktop.width
         img.style.height=imagesHeight.desktop.height
-        img.style.display="block"
+        img.style.display = "block"
+        img.style.cursor="pointer"
+        img.classList.add('media-gallery-images')
         const mediagallerySection = link.parentElement.parentElement.parentElement;
         mediagallerySection.innerHTML=""
         mediagallerySection.append(img)
     }); 
 
+    let firstRows, secRows, lastRows = null;
     const checkElementExist= Array.from(block.classList)
     
-    const firstRows=checkElementExist.includes('mg-two-one-row')
-    const secRows=checkElementExist.includes('mg-one-two-row')
-    const lastRows=checkElementExist.includes('mg-one-one-one-row')
+    firstRows=checkElementExist.includes('mg-two-one-row')
+    secRows=checkElementExist.includes('mg-one-two-row')
+    lastRows=checkElementExist.includes('mg-one-one-one-row')
 
     if(lastRows){
         block.classList.add('last-row')
@@ -84,10 +90,8 @@ export default function decorate(block) {
                 element.classList.add("right-img")
                 element.classList.add("ml")
                 const img = element.querySelector('img')
-                if(img){
-                    img.style.height = imagesHeight.lg.desktop.height
-                    img.style.width = imagesHeight.lg.desktop.width    
-                }
+                img.style.height = imagesHeight.lg.desktop.height
+                img.style.width = imagesHeight.lg.desktop.width
             }
 
         })
@@ -104,17 +108,83 @@ export default function decorate(block) {
                 element.remove()
             }else{
                 const img = element.querySelector('img')
-                if (img) {
-                    img.style.height = imagesHeight.lg.desktop.height
-                    img.style.width = imagesHeight.lg.desktop.width
-                    element.classList.add("mr")
-                    element.classList.add("right-img")
-                }
+                img.style.height = imagesHeight.lg.desktop.height
+                img.style.width = imagesHeight.lg.desktop.width
+                element.classList.add("mr")
+                element.classList.add("right-img")
+    
             }
         })
     
         block.append(wrapperDiv)
     }
     handleLayoutOnResize(block)
+    const modal = document.createElement('div')
+    modal.classList.add("media-gallery-modal")
+
+    const modalBody=document.createElement('div')
+    modalBody.classList.add("media-gallery-modal-body")
     
+    const modalImage=document.createElement('img')
+    modalImage.classList.add('media-gallery-body-img')
+
+    modalBody.append(modalImage)
+    const bodySubSection = document.createElement('div')
+    bodySubSection.innerHTML = `
+    <div class="media-gallery-modal-bottom-section">
+        <div>
+            <p class="media-gallery-modal-title">A scenic aerial view of Resorts World Sentosa</p>
+            <p class="media-gallery-modal-description">3 Feb 2025</p>
+        </div>
+        <a href="#" class="media-gallery-modal-btn"></a>
+    </div>
+    `
+    const downloadIcon = stringToHTML(SvgIcon({ name: 'downloadarrow', size: '16px' }));
+    bodySubSection.querySelector('.media-gallery-modal-btn').append(downloadIcon)
+
+    const closeIcon = stringToHTML(SvgIcon({ name: 'close', color: "#B29152", size: '16px' }));
+    
+    const closeBtn=document.createElement('button')
+    closeBtn.classList.add("media-gallery-modal--close-btn")
+    closeBtn.append(closeIcon)
+    modalBody.appendChild(closeBtn)
+
+    modalBody.append(bodySubSection)
+    modal.append(modalBody)
+
+    if (!document.querySelector('.media-gallery-modal'))
+        document.body.appendChild(modal)
+
+    document.querySelectorAll('.media-gallery-images').forEach((img)=>{
+
+        if (!img.hasAttribute('data-listener-added')) {
+            img.addEventListener('click', () => {
+                const modal = document.querySelector('.media-gallery-modal')
+                modal.style.visibility = 'visible'
+                document.body.style.position='relative'
+                const modalElement = document.querySelector('.media-gallery-modal-body')
+                const rect = modalElement.getBoundingClientRect();
+                const scrollTop =  document.documentElement.scrollTop;
+                const scrollLeft = document.documentElement.scrollLeft;
+                document.querySelector('.media-gallery-body-img').src=img.src
+                const top = rect.top + scrollTop - (window.innerHeight / 2) + (rect.height / 2);
+                const left = rect.left + scrollLeft - (window.innerWidth / 2) + (rect.width / 2);
+
+                window.scrollTo({
+                    top: top,
+                    left: left,
+                    behavior: 'smooth'
+                });
+                document.body.style.overflowY='hidden'
+            })
+        }
+            // Mark that the listener has been added
+            img.setAttribute('data-listener-added', 'true');
+    })
+
+    document.querySelector('.media-gallery-modal--close-btn').addEventListener('click',()=>{
+        const modal = document.querySelector('.media-gallery-modal')
+        modal.style.visibility = 'hidden'
+        document.body.style.overflowY = 'scroll'
+    })
 }
