9aba7e858a41de14680ca1a8fd96eaa03dfc3461
gaurav-ogilvy <gaurav.kumar@ogilvy.com>
2025-06-26 12:04:12 +0530

    Update footer.js

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -26,7 +26,7 @@ const handleAccordionToggle = (e, keyboardTrigger = false) => {
  */
 export default async function decorate(block) {
   // load footer as fragment
-  console.log('footer1');
+  console.log('footer-26June');
   const footerMeta = getMetadata('footer');
   const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
   const fragment = await loadFragment(footerPath);
